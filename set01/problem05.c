171a8a2817d0314379095fc77b543ddf74cb697d
unknown <nithyashrees.bsc23@rvu.edu.in>
2024-02-06 19:25:40 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+int input();
+int compare(int a,int b,int c,int largest );
+void output(int a,int b,int c,int largest);
+int main()
+{
+    int a, b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    largest=compare(a,b,c,largest);
+    output(a,b,c,largest);
+    return 0;
+}
+int input()
+{
+    int x;
+    printf("enter the values\n");
+    scanf("%d",&x);
+    return x;
+}
+int compare(int a,int b,int c,int largest )
+{
+  if(a>=b&&a>=c)
+  {
+    largest=a;
+    return a;
+  }
+  if(b>=a&&b>=c)
+  {
+    largest=b;
+    return a;
+  }
+  else{
+    largest=c;
+    return c;
+  }
+}
+void output(int a,int b,int c,int largest)
+{
+    printf("the largest of %d,%d,%d is %d\n",a,b,c,largest);
+}
 
