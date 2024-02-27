a2eb0bf49455f22880274c255545e178659bfc27
unknown <nithyashrees.bsc23@rvu.edu.in>
2024-02-27 13:57:50 +0530

    test

---
+++ set02/problem07.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+typedef struct _triangle {
+    float base, altitude, area;
+} Triangle;
+
+Triangle input_triangle();
+void find_area(Triangle *t);
+void output(Triangle t);
+
+int main() {
+    Triangle t = input_triangle();
+    find_area(&t);
+    output(t);
+    return 0;
+}
+
+Triangle input_triangle() {
+    Triangle t;
+    printf("Enter the base and altitude of the triangle: ");
+    scanf("%f %f", &t.base, &t.altitude);
+    return t;
+}
+
+void find_area(Triangle *t) {
+    t->area = 0.5 * t->base * t->altitude;
+}
+
+void output(Triangle t) {
+    printf("The area of triangle with base = %.6f and altitude = %.6f is %.6f\n", t.base, t.altitude,t.area);
+}
